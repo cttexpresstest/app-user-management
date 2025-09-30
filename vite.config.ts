56a9eb34d93adbb39e0eef9f8e3abb4260e571d8
Victor Ortuño Nuñez <victor.ortuno@cttexpress.com>
2025-09-30 12:34:09 +0200

    Fix source

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    tailwindcss(),
     react(),
+    tailwindcss(),
   ],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
+  
